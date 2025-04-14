884594aa818da3f836a8559c1e11eaea640dc05a
AndrewXadi <1998anat10@gmail.com>
2025-04-14 16:08:33 +0200

    version fix

--- __init__.py
+++
@@ -1,4 +0,0 @@
-"""Initialize the app"""
-
-__version__ = "0.0.14"
-__title__ = "BigBrother"
--- aa_bb/__init__.py
+++ aa_bb/__init__.py
@@ -1,4 +1,4 @@
 """Initialize the app"""
 
-__version__ = "0.0.13"
+__version__ = "0.0.15"
 __title__ = "BigBrother"
