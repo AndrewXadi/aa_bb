7f5d4c3e8501e1ac9e3ede84216941239410f796
AndrewXadi <1998anat10@gmail.com>
2025-04-22 15:36:52 +0200

    imp bl fix

--- aa_bb/checks/imp_blacklist.py
+++ aa_bb/checks/imp_blacklist.py
@@ -1,28 +1,32 @@
 from allianceauth.authentication.models import CharacterOwnership
+import urllib.parse
 
-def generate_blacklist_links(user_id, base_url="https://gice.goonfleet.com/Blacklist", max_url_length=500):
+def generate_blacklist_links(user_id, base_url="https://gice.goonfleet.com/Blacklist", max_url_length=2000):
     characters = CharacterOwnership.objects.filter(user__id=user_id)
     names = [str(char.character) for char in characters]
-    
+
     links = []
     current_names = []
 
     for name in names:
         test_list = current_names + [name]
-        query_string = ",".join(test_list)
+        query_string = urllib.parse.quote(",".join(test_list))  # URL-encode
         url = f"{base_url}?q={query_string}"
 
         if len(url) >= max_url_length:
-            # Finalize the current URL and start a new batch
-            link_label = "Click here" if not links else "and here"
-            links.append(f"<a href='{base_url}?q={','.join(current_names)}'>{link_label}</a>")
+            links.append(current_names)
             current_names = [name]
         else:
             current_names = test_list
 
-    # Add the final batch if anything's left
     if current_names:
-        link_label = "Click here" if not links else "and here"
-        links.append(f"<a href='{base_url}?q={','.join(current_names)}'>{link_label}</a>")
+        links.append(current_names)
 
-    return links
+    formatted_links = []
+    for i, chunk in enumerate(links):
+        link_text = "Click here" if i == 0 else "and here"
+        query = urllib.parse.quote(",".join(chunk))  # URL-encode
+        formatted_links.append(f"<a href='{base_url}?q={query}'>{link_text}</a>")
+
+    return formatted_links
+
